bcd9f628bd2607893cc3a25a0122ab5745611116
Anna Kartasheva <annakartashevamail@gmail.com>
2023-05-03 19:39:47 +0300

    Return all results instead of one

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def make_candidate(digits: str, fillers: List[str]) -> str:
     return "".join(chain.from_iterable(zip(digits, fillers))) + digits[-1]
 
 
-def find_match(fillers: Tuple[str, str, str], digits: str, target: int) -> chain | str:
+def find_match(fillers: Tuple[str, str, str], digits: str, target: int) -> set:
+    result = set()
     candidates = set()
     empty_spaces = len(digits) - 1
     possibilities = len(fillers) ** empty_spaces
@@ -15,12 +16,12 @@ def find_match(fillers: Tuple[str, str, str], digits: str, target: int) -> chain
         fillers_combination = [random.choice(fillers) for _ in range(empty_spaces)]
         candidate = make_candidate(digits, fillers_combination)
         if eval(candidate) == target:
-            return candidate
+            result.add(candidate)
         candidates.add(make_candidate(digits, fillers_combination))
-    return "Match not found"
+    return result
 
 
 if __name__ == "__main__":
     print(
-        f"Result: {find_match(digits='9876543210', fillers=('+', '-', ''), target=200)}"
+        *find_match(digits="9876543210", fillers=("+", "-", ""), target=200), sep="\n"
     )
